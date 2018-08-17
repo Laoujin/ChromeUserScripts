ea8cb93e30fdb268252106aedb6ef3f223a75010
Laoujin <woutervs@hotmail.com>
2018-08-17 22:28:56 +0200

    template

---
+++ template/template.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name
+// @namespace    https://itenium.be/
+// @author       Wouter Van Schandeijl
+// @match        https://
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  var element = document.getElementById('');
+})();
