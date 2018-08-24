4127abeae97975173b1e4ba7aa6e866577e33b9f
Laoujin <woutervs@hotmail.com>
2018-08-24 20:41:32 +0200

    more stackexchange urls

--- stackexchange/stackexchange.user.js
+++ stackexchange/stackexchange.user.js
@@ -4,6 +4,9 @@
 // @author       Wouter Van Schandevijl
 // @match        https://stackoverflow.com/
 // @match        https://*.stackexchange.com/
+// @match        https://superuser.com/
+// @match        https://serverfault.com/
+// @match        https://askubuntu.com/
 // @grant        none
 // ==/UserScript==
 
