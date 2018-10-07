499d142f6a3c3957af457d8eb3b0d1e3ae50b6a7
Laoujin <woutervs@hotmail.com>
2018-10-07 03:12:11 +0200

    template: https for jquery link

---
+++ password-revealer/README.md
@@ -0,0 +1,5 @@
+Password Revealer
+=================
+
+Add a "Show" button next to each `<input type="password">` to turn it into regular input text.
+So you can more easily recover any stored password.
--- template/template.user.js
+++ template/template.user.js
@@ -10,7 +10,7 @@
 // eslint-disable-next-line
 var load,execute,loadAndExecute;load=function(a,b,c){var d;d=document.createElement("script"),d.setAttribute("src",a),b!=null&&d.addEventListener("load",b),c!=null&&d.addEventListener("error",c),document.body.appendChild(d);return d},execute=function(a){var b,c;typeof a=="function"?b="("+a+")();":b=a,c=document.createElement("script"),c.textContent=b,document.body.appendChild(c);return c},loadAndExecute=function(a,b){return load(a,function(){return execute(b)})};
 var $;
-loadAndExecute('http://code.jquery.com/jquery-latest.js', function() {
+loadAndExecute('https://code.jquery.com/jquery-latest.js', function() {
   console.log($);
 });
 
