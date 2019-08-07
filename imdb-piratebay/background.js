568b6631c5742e81cf6bde278a21f9ab487bf969
Laoujin <woutervs@hotmail.com>
2019-08-07 22:20:23 +0200

    imdb: cleanup + update url

--- imdb-piratebay/background.js
+++ imdb-piratebay/background.js
@@ -4,29 +4,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 var piratebayUrl = 'https://pirateproxy.bet';
-
-// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-//   console.log('uhoh', tabId);
-//   console.log('2', changeInfo);
-//   console.log('3', tab);
-//   if (changeInfo.status == 'complete') {
-
-//     // do your things
-
-//   }
-// });
-
-// console.log('owhyeah', chrome);
-// console.log('zerfzef', window);
-
-// window.addEventListener("PassToBackground", function (evt) {
-//   alert(evt);
-//   chrome.runtime.sendMessage(evt.detail);
-// }, false);
-
-// chrome.tabs.executeScript(tab.id, { code: '$("body").append("Test : "' + text + ');' });
-
-// console.log('BG: getting settings');
 chrome.storage.sync.get({ piratebayUrl }, function (items) {
   piratebayUrl = items.piratebayUrl;
   console.log('BG: piratebayUrl from sets', piratebayUrl);
@@ -40,32 +17,14 @@ chrome.runtime.onMessage.addListener(
     console.log(sender.tab ?
       "from a content script:" + sender.tab.url :
       "from the extension");
+
+    if (request.piratebayUrl) {
+      piratebayUrl = request.piratebayUrl;
+      console.log('BG: piratebayUrl from sets update', piratebayUrl);
+    }
+
     if (request.greeting == "hello") {
       console.log('BG: Sending', piratebayUrl);
       sendResponse(piratebayUrl);
     }
   });
-
-
-// chrome.storage.sync.get({ piratebayUrl: defaultPiratebayUrl }, function (items) {
-//   // alert('what?')
-//   const event = new CustomEvent('piratebayUrl', {piratebayUrl: items.piratebayUrl});
-//   window.dispatchEvent(event);
-// });
-
-
-// chrome.runtime.onMessage.addListener(
-//   function (request, sender, sendResponse) {
-//     console.log("background.js got a message")
-//     console.log(request);
-//     console.log(sender);
-//     sendResponse("bar");
-//   }
-// );
-
-
-// chrome.tabs.sendMessage(tab.id, { content: "message" }, function (response) {
-//   if (response) {
-//     console.log('res', response);
-//   }
-// });
--- imdb-piratebay/imdb-piratebay.user.js
+++ imdb-piratebay/imdb-piratebay.user.js
@@ -6,31 +6,17 @@
 // ==/UserScript==
 
 /* eslint no-console: 0 */
-// eslint-disable-next-line
-// var load,execute,loadAndExecute;load=function(a,b,c){var d;d=document.createElement("script"),d.setAttribute("src",a),b!=null&&d.addEventListener("load",b),c!=null&&d.addEventListener("error",c),document.body.appendChild(d);return d},execute=function(a){var b,c;typeof a=="function"?b="("+a+")();":b=a,c=document.createElement("script"),c.textContent=b,document.body.appendChild(c);return c},loadAndExecute=function(a,b){return load(a,function(){return execute(b)})};
-// var $;
 
 var piratebayUrl = 'https://pirateproxy.bet';
 
-// console.log('CS: about to send message');
 chrome.runtime.sendMessage('eclmjjegilohajalkpdlepefflpkokpg', { greeting: "hello" }, function (response) {
   console.log('CS: received', response);
   piratebayUrl = response;
 });
 
 
-// chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
-//   console.log('contents');
-//   if (request.content) {
-//     sendResponse({ content: "response message" });
-//     return true; // This is required by a Chrome Extension
-//   }
-// })
-
-// console.log('pirate1', piratebayUrl);
 
 //loadAndExecute('https://code.jquery.com/jquery-latest.js', function() {
-// console.log('CS: pirate2', jQuery);
   function getMovieTitle() {
     const title = $('h1');
     if (title.length !== 1) {
@@ -67,30 +53,15 @@ chrome.runtime.sendMessage('eclmjjegilohajalkpdlepefflpkokpg', { greeting: "hell
 
   const widget = addDownloadButton();
   $('#piratebay-download', widget.parent()).on('click', function() {
-    // chrome.runtime.sendMessage('eclmjjegilohajalkpdlepefflpkokpg', 'requestPiratebayUrl', {}, function(url) {
-    //   console.log('url', url);
-    // });
 
-    console.log('BAY', piratebayUrl);
+  console.log('BAY', piratebayUrl);
 
 
-    // var message = {/* whatever */ };
-    // var event = new CustomEvent("PassToBackground", { detail: message });
-    // window.dispatchEvent(event);
+  const pirateUrl = piratebayUrl.replace(/\/*$/, '') + '/search/{searchTerm}/0/99/0';
+  const movieTitle = getMovieTitle().replace(/[^0-9a-zA-Z ]+/, '');
+  const searchTerm = encodeURI(movieTitle);
+  const url = pirateUrl.replace('{searchTerm}', searchTerm);
 
-    // window.addEventListener("piratebayUrl", function (evt) {
-    //   console.log('evt', evt);
-    //   chrome.runtime.sendMessage(evt.piratebayUrl);
-    // }, false);
-
-    // chrome.storage.sync.get({ piratebayUrl: defaultPiratebayUrl }, function (items) {
-    const pirateUrl = piratebayUrl.replace(/\/*$/, '') + '/search/{searchTerm}/0/99/0';
-    const movieTitle = getMovieTitle().replace(/[^0-9a-zA-Z ]+/, '');
-    const searchTerm = encodeURI(movieTitle);
-    const url = pirateUrl.replace('{searchTerm}', searchTerm);
-
-    console.log('getting', url);
-    window.open(url);
-    // });
-  });
-//});
+  console.log('getting', url);
+  window.open(url);
+});
