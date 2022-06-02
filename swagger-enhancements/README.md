a4b4ae40554157a5e12675cdba2862e82d59008d
Wouter Van Schandevijl <wouter.vanschandevijl@partena.be>
2022-06-02 19:54:31 +0200

    WIP: Set JWT more easily

--- swagger-enhancements/README.md
+++ swagger-enhancements/README.md
@@ -1,6 +1,5 @@
+Swagger-Enhancements
+====================
 
-
-
-
-Just include jquery to manifest.json::content_scripts and it will be available automatically?
---> check to no load twice... (itenium.be troubles overwriting laoded plugins)
+- Free text search
+- Easily Set JWT (WIP)
--- swagger-enhancements/swagger.user.js
+++ swagger-enhancements/swagger.user.js
@@ -51,5 +51,55 @@ loadAndExecute('https://code.jquery.com/jquery-latest.js', function() {
     });
   };
 
+
+
+  const addJwt = () => {
+    var authDiv = $('.auth-wrapper');
+    if (authDiv.length === 0) {
+      setTimeout(addJwt, 200);
+      return;
+    }
+
+    authDiv.prepend(`
+      <input type="text" id="jwtBearer" placeholder="Paste JWT here" style="padding: 5px 10px; margin: 0 4px 0 0;">
+    `);
+
+    const setJwt = jwtValue => {
+      const jwtInput = $(".auth-container input");
+      if (jwtInput.length === 0) {
+        setTimeout(setJwt.bind(this, jwtValue), 50);
+        return;
+      }
+
+      // Set Token
+      jwtInput.val(jwtValue);
+
+      // TODO: Because the value is being set in code, the event to enable the button is not executed
+      // You still need to actually focus the box, add a space and delete a space again before the button is clickable
+      // jwtInput.change();
+      // jwtInput.keyup();
+      // jwtInput.focus();
+
+      // Press the Auth button
+      $('.auth-btn-wrapper button.authorize').addClass('unlocked').enable().click();
+
+      // TODO: show how much longer before the JWT expires
+    };
+
+    $('#jwtBearer').change(function() {
+      let jwt = $(this).val();
+      if (jwt.toLowerCase().indexOf('bearer ') !== 0) {
+        jwt = 'Bearer ' + jwt;
+      }
+
+      // Open the Auth modal
+      $('.auth-wrapper button.authorize').click();
+      setTimeout(setJwt.bind(this, jwt), 50);
+    });
+  };
+
+
+
   setTimeout(addSearch, 200);
+  setTimeout(addJwt, 200);
 });
